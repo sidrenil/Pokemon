acd64bbf44641db467f7cefcf9601f2588864aab
sid <dengesidrenil@gmail.com>
2021-07-14 11:19:16 +0300

    init

--- app/src/main/java/com/example/pokemon/MainActivity.kt
+++ app/src/main/java/com/example/pokemon/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.pokemon
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.NestedScrollView
 import androidx.databinding.DataBindingUtil
 import com.example.pokemon.client.ApiClient
 import com.example.pokemon.databinding.ActivityMainBinding
@@ -14,34 +15,53 @@ import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
+    var page = 0
+    val limit = 20
     val pokemon = ArrayList<PokemonListResponse.Result>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        fetchPokemonData(0)
+    }
 
-        ApiClient.create().getNames().enqueue(object : retrofit2.Callback<PokemonListResponse> {
-            override fun onResponse(
-                call: retrofit2.Call<PokemonListResponse>,
-                response: Response<PokemonListResponse>,
-            ) {
-                val pokemonResponse = response.body()
-                pokemonResponse?.results?.let {
-                    if (response.isSuccessful && response.body() != null) {
-                        pokemon.addAll(it)
-                        setAdapter(pokemon)
-
-
+    private fun fetchPokemonData(offset: Int) {
+        ApiClient.create().getNames(limit, offset)
+            .enqueue(object : retrofit2.Callback<PokemonListResponse> {
+                override fun onResponse(
+                    call: retrofit2.Call<PokemonListResponse>,
+                    response: Response<PokemonListResponse>,
+                ) {
+                    val pokemonResponse = response.body()
+                    pokemonResponse?.results?.let {
+                        if (response.isSuccessful && response.body() != null) {
+                            pokemon.addAll(it)
+                            setAdapter(pokemon)
+                            setNestedScrollView()
+                        }
                     }
                 }
-            }
 
-            override fun onFailure(call: retrofit2.Call<PokemonListResponse>, t: Throwable) {
-                t.message?.let { Log.d("Error", it) }
-            }
+                override fun onFailure(call: retrofit2.Call<PokemonListResponse>, t: Throwable) {
+                    t.message?.let { Log.d("Error", it) }
+                }
 
+            })
+
+    }
+
+    private fun setNestedScrollView() {
+        binding.scrollNested.setOnScrollChangeListener(NestedScrollView.OnScrollChangeListener {
+                v,
+                scrollX,
+                scrollY,
+                oldScrollX,
+                oldScrollY,
+            ->
+            if (scrollY == v.getChildAt(0).measuredHeight - v.measuredHeight) {
+                page++
+                fetchPokemonData(limit * page)
+            }
         })
-
     }
 
     private fun setAdapter(pokemon: List<PokemonListResponse.Result>) {
